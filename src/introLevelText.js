304ce726246517de9b8d7053e3a1d7008e6a89a5
Joel <joelgoodrum@hotmail.com>
2018-12-10 21:42:04 -0500

    ENTER button works, needs to read input

--- src/introLevelText.js
+++ src/introLevelText.js
@@ -1,9 +1,18 @@
 function narrator1 () {
 	gameAction = "type your name and press enter.";
 	update();
+
+}
+
+function narrator2() {
+
+	name = "james";
+	gameAction = name + ", nice to meet you!"
+	update();
 }
 
 narrator1();
+
 
 
 								
